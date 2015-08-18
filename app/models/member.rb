e6c0904c0777d34a1fb81d7e971073972cbe0c65
katorie <katoriexxxkatorie@gmail.com>
2015-08-18 18:24:22 +0900

    Validation of member name

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,3 +1,4 @@
 class Member < ActiveRecord::Base
   has_many :shift_requests
+  validates :name, presence: true
 end
--- test/models/member_test.rb
+++ test/models/member_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class MemberTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Name is valid" do
+    member = FactoryGirl.create(:member)
+    assert member.valid?
+  end
+
+  test "Blank name is invalid" do
+    member = Member.new(name: ' ')
+    assert member.invalid?
+  end
 end
