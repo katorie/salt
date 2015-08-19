65c848b925a8a80d01e1072d2bb39e6239f6e69b
katorie <katoriexxxkatorie@gmail.com>
2015-08-19 23:07:36 +0900

    Fix enum

--- app/models/shift_request.rb
+++ app/models/shift_request.rb
@@ -1,5 +1,13 @@
 class ShiftRequest < ActiveRecord::Base
   belongs_to :member
   validates :member_id, presence: true
-  enum slot: %i(早番 遅番 泊り)
+  enum slot: %i(morning evening night)
+
+  def slot= value
+    if value.kind_of?(String) and value.to_i.to_s == value
+      super value.to_i
+    else
+      super value
+    end
+  end
 end
