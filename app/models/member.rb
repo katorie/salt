05194a6fc0537900dfc6e6b4d78e1f70d451a73c
katorie <katoriexxxkatorie@gmail.com>
2015-08-21 15:15:13 +0900

    Don't insert record without slot or comment

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,5 +1,5 @@
 class Member < ActiveRecord::Base
   has_many :shift_requests
-  accepts_nested_attributes_for :shift_requests
+  accepts_nested_attributes_for :shift_requests, reject_if: proc {|attributes| attributes['slot'].nil? && attributes['comment'].blank? }
   validates :name, presence: true
 end
