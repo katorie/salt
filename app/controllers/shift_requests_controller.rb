96b6f87c7d17dba84f84cf84719dff27f677b79e
katorie <katoriexxxkatorie@gmail.com>
2015-09-26 14:10:42 +0900

    Select and show start_time and end_tme with string data-type

--- app/controllers/shift_requests_controller.rb
+++ app/controllers/shift_requests_controller.rb
@@ -26,6 +26,8 @@ class ShiftRequestsController < ApplicationController
     @request_month = today.next_month
     @request_days = @request_month.all_month
     @request_days.count.times { @member.shift_requests.build }
+    # Where should we write this array?
+    @time_select = %w(1000 1030 1100 1130 1200 1230 1300 1330 1400 1430 1500 1530 1600 1630 1700 1730 1800 1830 1900 1930 2000 2030 2100 2130 2200 2230 2300 2330 0000 0030 0100 0130 0200 0230 0300 0330 0400 0430 0500 0530 0600 0630 0700 0730 0800 0830 0900 0930)
   end
 
   # GET /shift_requests/1/edit
