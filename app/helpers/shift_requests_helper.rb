ba81778da8559482ea69b12b60455b3618a3a0c8
katorie <katoriexxxkatorie@gmail.com>
2015-10-13 17:40:52 +0900

    Add class if it is holiday and change color

--- app/helpers/shift_requests_helper.rb
+++ app/helpers/shift_requests_helper.rb
@@ -1,5 +1,5 @@
 module ShiftRequestsHelper
-  def weekend?(day)
-    day.wday == 0 || day.wday == 6
+  def holiday?(day)
+    day.wday == 0 || day.wday == 6 || @national_holidays.any? {|d| d.date == day }
   end
 end
