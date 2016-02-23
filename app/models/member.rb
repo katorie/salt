645736c6336d066ca20bff49442a91d5b768c6e6
katorie <katoriexxxkatorie@gmail.com>
2016-02-23 21:08:04 +0900

    Modify edit and delete

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,5 +1,5 @@
 class Member < ActiveRecord::Base
   has_many :shift_requests
-  accepts_nested_attributes_for :shift_requests, reject_if: proc {|attributes| attributes['slot'].nil? && attributes['comment'].blank? }
+  accepts_nested_attributes_for :shift_requests, reject_if: proc {|attributes| attributes['slot'].nil? && attributes['comment'].blank? }, allow_destroy: true
   validates :name, presence: true, uniqueness: true
 end
