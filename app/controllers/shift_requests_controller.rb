501f3dfa2da739deb0ce40cfd4d9d6201790098b
katorie <katoriexxxkatorie@gmail.com>
2016-02-25 09:17:51 +0900

    Sort by date

--- app/controllers/shift_requests_controller.rb
+++ app/controllers/shift_requests_controller.rb
@@ -5,7 +5,7 @@ class ShiftRequestsController < ApplicationController
   # GET /shift_requests
   # GET /shift_requests.json
   def index
-    @shift_requests = ShiftRequest.all.includes(:member)
+    @shift_requests = ShiftRequest.all.includes(:member).order(:member_id).order(:date)
 
     respond_to do |format|
       format.html
@@ -25,8 +25,8 @@ class ShiftRequestsController < ApplicationController
     @shift_request = ShiftRequest.new
     today = Date.today
     @request_month = today.next_month
-    @applied_days = @member.shift_requests.map {|d| d.date }
-    @request_days = @request_month.all_month.to_a.delete_if {|d| @applied_days.include?(d) }
+    @applied_days = @member.shift_requests.map {|d| d.date }.sort
+    @request_days = @request_month.all_month.to_a.delete_if {|d| @applied_days.include?(d) }.sort
     @request_days.count.times { @member.shift_requests.build }
     # Where should we write this array?
     @time_select = %w(1000 1030 1100 1130 1200 1230 1300 1330 1400 1430 1500 1530 1600 1630 1700 1730 1800 1830 1900 1930 2000 2030 2100 2130 2200 2230 2300 2330 0000 0030 0100 0130 0200 0230 0300 0330 0400 0430 0500 0530 0600 0630 0700 0730 0800 0830 0900 0930)
